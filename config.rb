c8aa47abcedb317fe0e3f024edc3ba62fcc4ecd8
nizhu <nick@student.unsw.edu.au>
2013-07-28 13:34:11 +1000

    adding missing configs

--- config.rb
+++ config.rb
@@ -4,11 +4,11 @@ require 'sass-globbing'
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
+http_path = "/ruby-intro-site/"
+http_images_path = "/ruby-intro-site/images"
 http_generated_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_fonts_path = "/ruby-intro-site/fonts"
+css_dir = "public/ruby-intro-site/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
